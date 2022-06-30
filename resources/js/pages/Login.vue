e50abfbafa2d9678061d530babad991d061d7682
mattsershaw <masato.sasho@gmail.com>
2022-06-30 22:01:43 +0900

    ログイン機能

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -59,9 +59,16 @@ export default {
     }
   },
   methods: {
-    login () {
-      console.log(this.loginForm)
+        async login () {
+        // authストアのloginアクションを呼び出す
+        await this.$store.dispatch('auth/login', this.loginForm)
+
+        // トップページに移動する
+        this.$router.push('/')
     },
+    // login () {
+    //   console.log(this.loginForm)
+    // },
     async register () {
         // authストアのresigterアクションを呼び出す
         await this.$store.dispatch('auth/register', this.registerForm)

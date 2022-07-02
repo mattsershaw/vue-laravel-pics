44ade082c312a60a8068f241ba562a2682dd56c3
mattsershaw <masato.sasho@gmail.com>
2022-07-02 16:01:06 +0900

    写真投稿APIテストまで

--- routes/api.php
+++ routes/api.php
@@ -26,3 +26,5 @@ Route::post('/login', 'Auth\LoginController@login')->name('login');
 Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
 
 Route::get('/user', fn() => Auth::user())->name('user');
+
+Route::post('/photos', 'PhotoController@create')->name('photo.create');
